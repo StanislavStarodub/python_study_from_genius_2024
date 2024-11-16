ff26af6e05cf4917bd2f14de38d0da946cfcfb23
Stanislav Starodub <starodubss@gmail.com>
2024-11-16 15:33:11 +0200

    add the 5 lesson

---
+++ 5_lesson/module/main.py
@@ -0,0 +1,7 @@
+import utilities as util
+
+my_list = [23, 45, 1, -2, 34, 12, 66, 98, -45, 0]
+print('The average value of the my list is:', util.calculate_average(my_list))
+print('The max number of my list is:', util.find_max(my_list))
+print('The min number of my list is:', util.find_min(my_list))
+
---
+++ 5_lesson/module/utilities.py
@@ -0,0 +1,9 @@
+
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers) if len(numbers) != 0 else "The empty list. Exit!"
+
+def find_max(numbers):
+    return max(numbers)
+
+def find_min(numbers):
+    return min(numbers)
